6596312bb3dfc978429d543075b3984ee80c557c
Abinash Behera <Abinash.behera04@gmail.com>
2017-07-17 02:00:36 +0530

    p5
    '''Take two lists, say for example these two:
    
      a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
      b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
    and write a program that returns a list that contains only the elements
    that are common between the lists (without duplicates).
    Make sure your program works on two lists of different sizes.
    
    Extras:
    
    Randomly generate two lists to test this
    Write this in one line of Python'''

--- p4.py
+++ p4.py
@@ -1,14 +1,10 @@
-'''Create a program that asks the user for a number and then prints
- out a list of all the divisors of that number.
- (If you don’t know what a divisor is, it is a number that divides 
-evenly into another number. For example, 13 is a divisor of 26 
-because 26 / 13 has no remainder.)'''
+'''Take two lists, say for example these two:
 
-num = int(raw_input('Enter a number \n'))
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
 
-div = []
-for i in range(1,num+1):
-    if num % i == 0:
-        div.append(i)
+Extras:
 
-print div
+Randomly generate two lists to test this
+Write this in one line of Python'''
