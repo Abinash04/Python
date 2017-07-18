2340cbdce04a6139159f90893e237d7e80116810
Abinash Behera <Abinash.behera04@gmail.com>
2017-07-19 02:26:05 +0530

    completed p6.py

--- p6.py
+++ p6.py
@@ -1,9 +1,18 @@
 #Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 
-num = raw_input('Enter a string to find if its a PALINDROME?')
-print 'The sttring is:', num
-rev = reverse(num)
+num = raw_input('Enter a string to find if its a PALINDROME ?\n')
+print 'The string is:', num
+rev = ''.join(reversed(num))
 
 print 'Palindrome ',rev
+
+'''Another way of finding reverse
+rev1 = num [::-1]
+print rev1'''
+
+if num == rev1:
+    print 'Palindrome'
+else:
+    print "Not a Palindrome"
 
 
