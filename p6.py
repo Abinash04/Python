5282af0a737a7cfe2ba10424e608b8187990f1ba
Abinash Behera <Abinash.behera04@gmail.com>
2017-07-19 02:15:06 +0530

    added print line of the string

--- p6.py
+++ p6.py
@@ -1,8 +1,9 @@
 #Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 
 num = raw_input('Enter a string to find if its a PALINDROME?')
-
+print 'The sttring is:', num
 rev = reverse(num)
 
 print 'Palindrome ',rev
+
 
