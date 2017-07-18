792db28b10a0437b38894a5b5c0d6d9bc11d5cff
Abinash Behera <Abinash.behera04@gmail.com>
2017-07-19 01:31:03 +0530

    p5

--- p5.py
+++ p5.py
@@ -11,19 +11,47 @@ Extras:
 Randomly generate two lists to test this
 Write this in one line of Python'''
 
+import random
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-len1 = len(a)
-len2 = len(b)
 c = []
+print 'List in a: ',a
+for i in a:
+    if i in b:
+        try:
+          c.append(i)
+        except:
+            pass
 
-for i in range(0,len2+1):
-    if a[i]  b[i]:
-        c.append(a[i])
-        a[i+1]
-    else:
-        pass
+print set(c)
+rnum = []
+for k in range(1,10):
+    rand_num = random.randint(1,99)
+    rnum.append(rand_num)
 
-print c
+print 'random list: ',rnum
+print 'list in b: ',b
+d = []
+for i in rnum:
+    if i in b:
+        try:
+            d.append(j)
+        except:
+            pass
+print d
+
+one=random.sample(range(1,100),25)
+print one
+two=random.sample(range(12,67),15)
+print two
+f = []
+for m in one:
+    if m in two:
+      f.append(m)
+
+print 'matched list:', f
+
+g = [ item for item in a if (item in b)]
+print set(g)
         
