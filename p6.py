55906e8924474b6e855aa0c08595ad62d8342001
Abinash Behera <Abinash.behera04@gmail.com>
2017-07-19 01:57:01 +0530

    p6
    Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
    +

---
+++ p6.py
@@ -0,0 +1,8 @@
+#Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+
+num = raw_input('Enter a string to find if its a PALINDROME?')
+
+rev = reverse(num)
+
+print rev
+
